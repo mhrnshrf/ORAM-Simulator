5dd67728cc890383561f0f2e4636152fe36a9127
Mehrnoosh <mhrnshrf@gmail.com>
2020-07-29 18:54:50 -0700

    final experiment begins

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 TRACEFILE="$1"
 LOGNAME="$2"
-/mnt/e/Pitt/Summer\ 2020/ORAM-Simulator/bin/usimm input/4channel.cfg /mnt/c/trace/"$TRACEFILE"  > /mnt/e/Pitt/Summer\ 2020/ORAM-Simulator/log/"$LOGNAME.txt"
+/mnt/e/Pitt/Summer\ 2020/ORAM-Simulator/bin/usimm input/1channel.cfg /mnt/c/trace/"$TRACEFILE"  > /mnt/e/Pitt/Summer\ 2020/ORAM-Simulator/log/"$LOGNAME.txt"
