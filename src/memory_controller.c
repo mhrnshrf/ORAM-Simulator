ed05a0541146cbb1b9e2cbb17dfbd4b7f938b02c
Mehrnoosh <mhrnshrf@gmail.com>
2020-08-08 11:39:29 -0700

    wb miss

--- src/memory_controller.c
+++ src/memory_controller.c
@@ -2552,7 +2552,7 @@ void early_writeback(){
   if (LLC[i][j].valid && LLC[i][j].dirty)
   {
     posblk = pos_calc(block_addr(LLC[i][j].addr), 1);
-    plb_print(posblk);
+    // plb_print(posblk);
     if (plb_access(posblk))
     {
       // plb_print(posblk);
