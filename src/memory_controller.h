bae0f56240c5d5cbaae49ab87f0af997286ca2b3
Mehrnoosh <mhrnshrf@gmail.com>
2020-08-15 15:03:36 -0700

    wb5

--- src/memory_controller.h
+++ src/memory_controller.h
@@ -18,7 +18,7 @@
 #define L2_LATENCY 10               // L2 latency in terms of # cycles 
 #define MAINMEM_LATENCY 0           // L2 latency in terms of # cycles 
 #define WARMUP_THRESHOLD 3000000    // L2 warm up threshold, after which stats are gathered and memory accesses are actully made
-#define TIMEOUT_THRESHOLD 5000      // time out threshold in seconds
+#define TIMEOUT_THRESHOLD 6000      // time out threshold in seconds
 #define TOP_BOUNDRY 10              // top region tree boundry
 #define MID_BOUNDRY 20              // middle region tree boundry
 
@@ -29,10 +29,10 @@
 #define STT_ENABLE 0         // 0/1 flag to disable/enable stash top tree  ~> it won't matter if volcano is enabled
 #define STL_ENABLE 0         // 0/1 flag to disable/enable slim tree level ~> it won't matter if volcano is enabled
 #define WRITE_BYPASS 0       // 0/1 flag to disable/enable cacheing the path id along the data in the LLC which will benefit write reqs to bypass posmap lookup 
-#define RHO_ENABLE 1         // 0/1 flag to disable/enable having rho
-#define TIMING_ENABLE 0      // 0/1 flag to disable/enable having timing channel security
+#define RHO_ENABLE 0         // 0/1 flag to disable/enable having rho
+#define TIMING_ENABLE 1      // 0/1 flag to disable/enable having timing channel security
 #define PREFETCH_ENABLE 0    // 0/1 flag to disable/enable having prefetching option in case of having timing channel security
-#define EARLY_ENABLE 0       // 0/1 flag to disable/enable early eviction option in case of having timing channel security
+#define EARLY_ENABLE 1       // 0/1 flag to disable/enable early eviction option in case of having timing channel security
 #define SNAPSHOT_ENABLE 0    // 0/1 flag to disable/enable performing snapshot by making path oram accesses
 #define TIMEOUT_ENBALE 1     // 0/1 flag to disable/enable finishing the program in case it get stuck
 
