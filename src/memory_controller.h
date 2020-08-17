8f0286350f221ad7c42ed3134d77a51feed0fb9c
Mehrnoosh <mhrnshrf@gmail.com>
2020-08-16 23:37:04 -0700

    t1k

--- src/memory_controller.h
+++ src/memory_controller.h
@@ -30,7 +30,7 @@
 #define STL_ENABLE 0         // 0/1 flag to disable/enable slim tree level ~> it won't matter if volcano is enabled
 #define WRITE_BYPASS 0       // 0/1 flag to disable/enable cacheing the path id along the data in the LLC which will benefit write reqs to bypass posmap lookup 
 #define RHO_ENABLE 0         // 0/1 flag to disable/enable having rho
-#define TIMING_ENABLE 0     // 0/1 flag to disable/enable having timing channel security
+#define TIMING_ENABLE 1     // 0/1 flag to disable/enable having timing channel security
 #define PREFETCH_ENABLE 0    // 0/1 flag to disable/enable having prefetching option in case of having timing channel security
 #define EARLY_ENABLE 0       // 0/1 flag to disable/enable early eviction option in case of having timing channel security
 #define SNAPSHOT_ENABLE 0    // 0/1 flag to disable/enable performing snapshot by making path oram accesses
