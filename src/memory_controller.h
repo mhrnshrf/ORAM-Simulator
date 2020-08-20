b23ebead52048853c9247b236cdaf87f1b4a2c9c
Mehrnoosh <mhrnshrf@gmail.com>
2020-08-19 19:57:58 -0700

    rho plus ir

--- src/memory_controller.h
+++ src/memory_controller.h
@@ -23,18 +23,18 @@
 #define MID_BOUNDRY 20              // middle region tree boundry
 
 // enable/disable options config
-#define TIMEOUT_ENBALE 0     // 0/1 flag to disable/enable finishing the program in case it get stuck
+#define TIMEOUT_ENBALE 1     // 0/1 flag to disable/enable finishing the program in case it get stuck
 #define SUBTREE_ENABLE 1     // 0/1 flag to diable/enable having subtree adddressing scheme
 #define CACHE_ENABLE 1       // 0/1 flag to diable/enable having cache
 #define VOLCANO_ENABLE 0     // 0/1 flag to disable/enable having volcano idea both stt and stl
-#define STT_ENABLE 0         // 0/1 flag to disable/enable stash top tree  ~> it won't matter if volcano is enabled
+#define STT_ENABLE 1         // 0/1 flag to disable/enable stash top tree  ~> it won't matter if volcano is enabled
 #define STL_ENABLE 1         // 0/1 flag to disable/enable slim tree level ~> it won't matter if volcano is enabled
 #define WRITE_BYPASS 0       // 0/1 flag to disable/enable cacheing the path id along the data in the LLC which will benefit write reqs to bypass posmap lookup 
-#define RHO_ENABLE 0         // 0/1 flag to disable/enable having rho
-#define TIMING_ENABLE 0     // 0/1 flag to disable/enable having timing channel security
+#define RHO_ENABLE 1         // 0/1 flag to disable/enable having rho
+#define TIMING_ENABLE 1     // 0/1 flag to disable/enable having timing channel security
 #define PREFETCH_ENABLE 0    // 0/1 flag to disable/enable having prefetching option in case of having timing channel security
-#define EARLY_ENABLE 0       // 0/1 flag to disable/enable early eviction option in case of having timing channel security
-#define SNAPSHOT_ENABLE 1    // 0/1 flag to disable/enable performing snapshot by making path oram accesses
+#define EARLY_ENABLE 1       // 0/1 flag to disable/enable early eviction option in case of having timing channel security
+#define SNAPSHOT_ENABLE 0    // 0/1 flag to disable/enable performing snapshot by making path oram accesses
 
 
 
