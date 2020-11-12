8bebfb02dadb06e2f799bb221d49d78e63495b6c
Mehrnoosh <mhrnshrf@gmail.com>
2020-11-11 19:07:50 -0500

    limit on stash linger 60 قث

--- src/memory_controller.h
+++ src/memory_controller.h
@@ -54,7 +54,7 @@
 #define U 0.50 // utilization
 #define RL 6     // # the reserved level
 #define STASH_SIZE_ORG 200     // original size of stash
-#define BK_EVICTION 1   // 0/1 flag to disable/enable background eviction
+#define BK_EVICTION 0   // 0/1 flag to disable/enable background eviction
 #define TOP_CACHE 10   // # top levels that are cached ---------- freecursive: 10, volcano: don't care
 #define L1 9   // upto L1 level buckts have specific Z1 number of slots   (inclusive)
 #define L2 15   // upto L2 level buckts have specific Z2 number of slots   (inclusive)
