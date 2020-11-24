7ee1a76203844e791528b732d6f35d5c1ea12181
Mehrnoosh <mhrnshrf@gmail.com>
2020-11-24 02:31:29 -0500

    main mem 200 added nonsec

--- src/memory_controller.h
+++ src/memory_controller.h
@@ -16,7 +16,7 @@
 #define PAGE_SIZE 4096              // page size in byte ~~~> 4KB
 #define L1_LATENCY 3                // L1 latency in terms of # cycles 
 #define L2_LATENCY 10               // L2 latency in terms of # cycles 
-#define MAINMEM_LATENCY 0           // mem latency in terms of # cycles 
+#define MAINMEM_LATENCY 200           // mem latency in terms of # cycles 
 #define WARMUP_THRESHOLD 3000000    // L2 warm up threshold, after which stats are gathered and memory accesses are actully made
 #define TIMEOUT_THRESHOLD 10000      // time out threshold in seconds
 #define TOP_BOUNDRY 10              // top region tree boundry
@@ -35,7 +35,7 @@
 #define PREFETCH_ENABLE 0     // 0/1 flag to disable/enable having prefetching option in case of having timing channel security
 #define EARLY_ENABLE    0     // 0/1 flag to disable/enable early eviction option in case of having timing channel security
 #define SNAPSHOT_ENABLE 0     // 0/1 flag to disable/enable performing snapshot by making path oram accesses
-#define NONSEC_ENABLE   0    // 0/1 flag to disable/enable oram simulation if off usimm runs normally
+#define NONSEC_ENABLE   1    // 0/1 flag to disable/enable oram simulation if off usimm runs normally
 
 
 
