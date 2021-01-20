d7f6ac227e2cff69a075a263574d5e08627eae8e
unknown <mhrnshrf@gmail.com>
2021-01-20 18:53:20 -0500

    test all batch log addr sleep

--- batch.sh
+++ batch.sh
@@ -18,6 +18,7 @@ bin/usimm input/"1channel.cfg" ../oram/trace/imagick  $LOGNAME > ../oram/log/"$L
 #bin/usimm input/"1channel.cfg" ../oram/trace/nab  $LOGNAME > ../oram/log/"$LOGNAME-nab.txt" &
 bin/usimm input/"1channel.cfg" ../oram/trace/fotonik3d  $LOGNAME > ../oram/log/"$LOGNAME-fotonik3d.txt" &
 bin/usimm input/"1channel.cfg" ../oram/trace/roms  $LOGNAME > ../oram/log/"$LOGNAME-roms.txt" ;
+sleep 2m
 cd ../oram/log; 
 python merge.py $LOGNAME;
 rm $LOGNAME-*.csv
