b48a7721fb90eb1faf23063df2e457c84cbbe8db
Mehrnoosh <mhrnshrf@gmail.com>
2021-01-26 23:57:53 -0500

    mid 24 buf 300

--- src/memory_controller.h
+++ src/memory_controller.h
@@ -121,7 +121,7 @@
 #define DEP_TH 5             // threshold on # reshuffle for dynamic ep 
 #define WL_CAP 100000        // cap on wl feature
 #define STALE_CAP 40          // cap on stale for each 5 levels
-#define STALE_BUF_SIZE 3000   //size of stale buffer
+#define STALE_BUF_SIZE 300   //size of stale buffer
 #define GL_COUNT 3          // # gathering levels
 #define GLMAX 15          // last level gl
 
