a9f0906a561cbdc5bd038fffee9051b9298c927d
Mehrnoosh <mhrnshrf@gmail.com>
2021-03-04 16:19:15 -0500

    fetch script fixed

--- fetch.sh
+++ fetch.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 #CONFIGILE="$1"
-cd ../ORAM-Simulator; rm obj/*; rm bin/usimm; git reset --hard HEAD; git pull; rm bin/usimm; cd src; make; cd ..; 
+LOGNAME="$1"
+cd ../ORAM-Simulator; rm obj/*; rm bin/usimm; git reset --hard HEAD; git pull; #rm bin/usimm; cd src; make; cd ..; 
+#bin/usimm input/"1channel.cfg" ../oram/trace/perlbench  $LOGNAME > ../oram/log/"$LOGNAME-perlbench.txt" &
