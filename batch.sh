6ad84b994aad58bc55bb8a663a577e5fbe8228d5
Mehrnoosh <mhrnshrf@gmail.com>
2021-03-05 11:00:42 -0500

    scripts fixed rstl + wskip

--- batch.sh
+++ batch.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #CONFIGILE="$1"
 LOGNAME="$1"
-cd ../ORAM-Simulator; rm obj/*; rm bin/usimm; cd src; make; cd ..; 
+cd ../ORAM-Simulator; # rm obj/*; rm bin/usimm; cd src; make; cd ..; 
 #bin/usimm input/"1channel.cfg" ../oram/trace/perlbench  $LOGNAME > ../oram/log/"$LOGNAME-perlbench.txt" &
 bin/usimm input/"1channel.cfg" ../oram/trace/gcc  $LOGNAME > ../oram/log/"$LOGNAME-gcc.txt" &
 bin/usimm input/"1channel.cfg" ../oram/trace/mcf  $LOGNAME > ../oram/log/"$LOGNAME-mcf.txt" &
