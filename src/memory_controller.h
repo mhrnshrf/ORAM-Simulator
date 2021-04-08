4b9a3e0a48b9758d7068e20f650fa491ffdfe22c
Mehrnoosh <mhrnshrf@gmail.com>
2021-04-08 16:50:22 -0400

    util avg overall stl only

--- src/memory_controller.h
+++ src/memory_controller.h
@@ -30,7 +30,7 @@
 // #define SUBTREE_ENABLE  0     // 0/1 flag to disable/enable having subtree adddressing scheme
 // #define CACHE_ENABLE    0     // 0/1 flag to disable/enable having cache
 #define VOLCANO_ENABLE  0     // 0/1 flag to disable/enable having volcano idea both stt and stl
-#define STT_ENABLE      1     // 0/1 flag to disable/enable stash top tree  ~> it won't matter if volcano is enabled
+#define STT_ENABLE      0     // 0/1 flag to disable/enable stash top tree  ~> it won't matter if volcano is enabled
 #define STL_ENABLE      1     // 0/1 flag to disable/enable slim tree level ~> it won't matter if volcano is enabled
 // #define WRITE_BYPASS    0     // 0/1 flag to disable/enable cacheing the path id along the data in the LLC which will benefit write reqs to bypass posmap lookup 
 // #define RHO_ENABLE      0     // 0/1 flag to disable/enable having rho
@@ -64,8 +64,8 @@
 #define STASH_SIZE_ORG 200     // original size of stash
 // #define TOP_CACHE 10   // # top levels that are cached ---------- freecursive: 10, volcano: don't care
 #define L1 9   // upto L1 level buckts have specific Z1 number of slots   (inclusive)
-#define L2 15   // upto L2 level buckts have specific Z2 number of slots   (inclusive)
-#define L3 18   // upto L3 level buckts have specific Z3 number of slots   (inclusive)
+#define L2 13   // upto L2 level buckts have specific Z2 number of slots   (inclusive)
+#define L3 17   // upto L3 level buckts have specific Z3 number of slots   (inclusive)
 #define CAP_LEVEL 20 // level where cap counter are maintaned
 
 // subtree invariant
@@ -135,8 +135,8 @@ enum{
   Z = (RING_ENABLE) ? RING_Z+RING_S : USUAL_Z,
   EMPTY_TOP = (VOLCANO_ENABLE || STT_ENABLE) ? 10 : 0,
   Z1 = (VOLCANO_ENABLE || STT_ENABLE) ? 0 : (RING_ENABLE && RSTL_ENABLE)? Z: Z,   // # slots per bucket upto L1    Z-5
-  Z2 = (VOLCANO_ENABLE || STL_ENABLE) ? 2 :(RING_ENABLE && RSTL_ENABLE)? Z: Z,   // # slots per bucket upto L2
-  Z3 = (VOLCANO_ENABLE || STL_ENABLE) ? 3 : (RING_ENABLE &&RSTL_ENABLE)? Z:Z,   // # slots per bucket upto L3   Z-10
+  Z2 = (VOLCANO_ENABLE || STL_ENABLE) ? 1 :(RING_ENABLE && RSTL_ENABLE)? Z: Z,   // # slots per bucket upto L2
+  Z3 = (VOLCANO_ENABLE || STL_ENABLE) ? 2 : (RING_ENABLE &&RSTL_ENABLE)? Z:Z,   // # slots per bucket upto L3   Z-10
   Z4 = (RING_ENABLE && !RSTL_ENABLE) ? Z3 : (RING_ENABLE && RSTL_ENABLE)? Z-5: Z,
   PATH = (long long int)pow(2,LEVEL-1),  // # paths in oram tree
   NODE = (long long int)pow(2,LEVEL)-1,  // # nodes in oram tree
