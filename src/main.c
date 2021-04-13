2c72208cc1547720295843e83b358975783fe7ee
Mehrnoosh <mhrnshrf@gmail.com>
2021-04-12 22:39:28 -0400

    nvm start 22 fixed

--- src/main.c
+++ src/main.c
@@ -1240,7 +1240,7 @@ int main(int argc, char * argv[])
 				while ((no_miss_occured && !expt_done) || (!SIM_ENABLE_VAR && tracectr < TRACE_SIZE-3) )
 				{
 					// printf("@ trace %d\n", tracectr);
-					if (!SIM_ENABLE_VAR && tracectr >= WARMUP_TREE)
+					if (SIM_ENABLE && !SIM_ENABLE_VAR && tracectr >= WARMUP_TREE)
 					{
 						switch_sim_enable_to(SIM_ENABLE);
 						switch_cache_enable_to(CACHE_ENABLE);
