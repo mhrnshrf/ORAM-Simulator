209811a1b03237fdd7ad63c8f35cd0ae2cae745c
Mehrnoosh <mhrnshrf@gmail.com>
2021-10-24 20:24:25 -0400

    calc super print

--- src/main.c
+++ src/main.c
@@ -1615,10 +1615,10 @@ int main(int argc, char * argv[])
 				// printf("cache enable if: @ trace %d\n", tracectr);
 				while ((no_miss_occured && !expt_done) || (!SIM_ENABLE_VAR && tracectr < TRACE_SIZE-3) ) //  && tracectr <= endpoint
 				{
-					// if (tracectr % 1000000 == 0)
-					// {
-						// printf("%d\n", tracectr);
-					// }
+					if (tracectr % 1000000 == 0)
+					{
+						printf("%d\n", tracectr);
+					}
 					// if (tracectr % 50000 == 0)
 					// {
 						// printf("@ %d no miss\n", tracectr);
