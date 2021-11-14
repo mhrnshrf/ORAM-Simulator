f4a33cfe0b0c7548342ea07dd2c59b66d5a57919
Mehrnoosh <mhrnshrf@gmail.com>
2021-11-14 11:52:32 -0500

    only last level s5

--- src/memory_controller.h
+++ src/memory_controller.h
@@ -65,7 +65,7 @@
 // #define TOP_CACHE 10   // # top levels that are cached ---------- freecursive: 10, volcano: don't care
 #define L1 9   // upto L1 level buckts have specific Z1 number of slots   (inclusive)
 #define L2 16   // upto L2 level buckts have specific Z2 number of slots   (inclusive)  // l24 ->17
-#define L3 18   // upto L3 level buckts have specific Z3 number of slots   (inclusive) //  l24 ->22
+#define L3 22   // upto L3 level buckts have specific Z3 number of slots   (inclusive) //  l24 ->22
 #define CAP_LEVEL 20 // level where cap counter are maintaned
 
 // subtree invariant
@@ -142,8 +142,8 @@ enum{
   Z = (RING_ENABLE) ? RING_Z+RING_S : USUAL_Z,
   EMPTY_TOP = (VOLCANO_ENABLE || STT_ENABLE) ? 10 : 0,
   Z1 = (VOLCANO_ENABLE || STT_ENABLE) ? 0 : (RING_ENABLE && RSTL_ENABLE)? Z: Z,   // # slots per bucket upto L1    Z-5
-  Z2 = (VOLCANO_ENABLE || STL_ENABLE) ? 2 : (RING_ENABLE && RSTL_ENABLE)? Z-2: Z,   // # slots per bucket upto L2
-  Z3 = (VOLCANO_ENABLE || STL_ENABLE) ? 2 : (RING_ENABLE && RSTL_ENABLE)? Z-2:Z,   // # slots per bucket upto L3   Z-10
+  Z2 = (VOLCANO_ENABLE || STL_ENABLE) ? 2 : (RING_ENABLE && RSTL_ENABLE)? Z: Z,   // # slots per bucket upto L2
+  Z3 = (VOLCANO_ENABLE || STL_ENABLE) ? 2 : (RING_ENABLE && RSTL_ENABLE)? Z:Z,   // # slots per bucket upto L3   Z-10
   Z4 = (RING_ENABLE && !RSTL_ENABLE) ? Z3 : (RING_ENABLE && RSTL_ENABLE)? Z-2: Z,
   PATH = (long long int)pow(2,LEVEL-1),  // # paths in oram tree
   NODE = (long long int)pow(2,LEVEL)-1,  // # nodes in oram tree
