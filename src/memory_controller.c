9b126aa95c1f56459d89928daf23c95f45957bcf
Mehrnoosh <mhrnshrf@gmail.com>
2021-11-25 16:04:55 -0500

    add to queue at remote access

--- src/memory_controller.c
+++ src/memory_controller.c
@@ -4721,47 +4721,6 @@ void gather_dead(int index, int i){
           
         }
       }
-      if (GlobTree[index].slot[j].redirect)
-      {
-        int rind = GlobTree[index].slot[j].remote_index;
-        int roff = GlobTree[index].slot[j].remote_offset;
-        if (GlobTree[rind].slot[roff].dd == DEAD && GlobTree[rind].allctr < REMOTE_CAP)
-        {
-          Element *db = (Element*) malloc(sizeof (Element));
-          db->index = rind;
-          db->offset = roff;
-
-          if (deadQ_arr[i]->size < deadQ_arr[i]->limit)
-          {
-            GlobTree[rind].slot[roff].dd = REMEMBERED;
-            GlobTree[rind].allctr++;
-
-            deadrem++;
-            Enqueue(deadQ_arr[i] , db);
-            dead_gathered[i]++;
-          }
-          else
-          {
-            if (deadQ_shadow[i]->size < deadQ_shadow[i]->limit)
-            {
-              Enqueue(deadQ_shadow[i] , db);
-              dead_shadowed[i]++;
-            }
-            else
-            {
-              deadQ_ov[i]++;
-              free(db);
-              break;
-            }
-
-          }
-          
-        }
-
-
-        
-      }
-      
     }
   }
 }
@@ -5120,6 +5079,44 @@ int remote_access(int index, int offset, int level){
   if (offset_redir >= LZ[level])
   {
     surplus_in_use--;
+  }
+
+  if (level >= GATHER_START)
+  {
+    int rind = index_redir;
+    int roff = offset_redir;
+    if (GlobTree[rind].slot[roff].dd == DEAD && GlobTree[rind].allctr < REMOTE_CAP)
+    {
+      Element *db = (Element*) malloc(sizeof (Element));
+      db->index = rind;
+      db->offset = roff;
+
+      if (deadQ_arr[level]->size < deadQ_arr[level]->limit)
+      {
+        GlobTree[rind].slot[roff].dd = REMEMBERED;
+        GlobTree[rind].allctr++;
+
+        deadrem++;
+        Enqueue(deadQ_arr[level] , db);
+        dead_gathered[level]++;
+      }
+      else
+      {
+        if (deadQ_shadow[level]->size < deadQ_shadow[level]->limit)
+        {
+          Enqueue(deadQ_shadow[level] , db);
+          dead_shadowed[level]++;
+        }
+        else
+        {
+          deadQ_ov[level]++;
+          free(db);
+          // break;
+        }
+
+      }
+      
+    }
   }
   
   return mem_addr;
