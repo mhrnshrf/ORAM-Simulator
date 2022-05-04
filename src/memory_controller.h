bf02a839bd0c922e8477ca8fbbf195567f4d8e7c
Mehrnoosh <mhrnshrf@gmail.com>
2022-05-03 20:33:41 -0400

    stale th buf 600 re

--- src/memory_controller.h
+++ src/memory_controller.h
@@ -410,8 +410,8 @@ extern int dram_to_serve_r_w;
 extern int nvm_to_serve_r_r;
 extern int nvm_to_serve_r_w;
 
-extern unsigned long int rctr = 0;
-extern unsigned long int wctr = 0;
+extern unsigned long int rctr;
+extern unsigned long int wctr;
 
 
 
