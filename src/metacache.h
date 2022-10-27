e66732a3e566eb605b46897d76c3f66d320bc193
Mehrnoosh <mhrnshrf@gmail.com>
2022-10-27 00:00:27 -0400

    sit rerun metacache no victim re 128

--- src/metacache.h
+++ src/metacache.h
@@ -5,11 +5,12 @@
 #include <stdbool.h>
 #include <math.h>
 
-#define METACACHE_SIZE 2097152  // in bytes ~~~> 2 MB
+// #define METACACHE_SIZE 2097152  // in bytes ~~~> 2 MB
 // #define METACACHE_SIZE 4194304  // in bytes ~~~> 4 MB
 // #define METACACHE_SIZE 1048576     // in bytes ~~~> 1 MB
-// #define METACACHE_SIZE 262144  // in bytes ~~~> 256 KB
 // #define METACACHE_SIZE 524288  // in bytes ~~~> 512 KB
+// #define METACACHE_SIZE 262144  // in bytes ~~~> 256 KB
+#define METACACHE_SIZE 131072  // in bytes ~~~> 128 KB
 #define META_NUM_WAY 16           //  ~~~> # way per set
 // #define META_NUM_WAY 8           //  ~~~> # way per set
 #define META_BLOCK_SIZE 64       // bytes ~~~> cacheline size
