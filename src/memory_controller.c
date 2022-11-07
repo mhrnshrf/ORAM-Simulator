d7d378245f5bcb5a2dad751d42e67696813b841d
Mehrnoosh <mhrnshrf@gmail.com>
2022-11-07 13:41:14 -0500

    skip turn inverse

--- src/memory_controller.c
+++ src/memory_controller.c
@@ -5154,7 +5154,8 @@ void ring_access(int addr){
     int b4 = stalectr;
     
     if(SKIP_ENABLE){
-      SKIP_ENABLE_VAR = (ring_evictctr % SKIP_TURN == 0) ? true : false;
+      // SKIP_ENABLE_VAR = (ring_evictctr % SKIP_TURN == 0) ? true : false;
+      SKIP_ENABLE_VAR = (ring_evictctr % SKIP_TURN != 0) ? true : false;
     }
 
     ring_evict_path(label);
