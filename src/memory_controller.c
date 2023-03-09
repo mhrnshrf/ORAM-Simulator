442ff56adb3d2f32ec8a57ba64cd59ffd6794eed
Mehrnoosh <mhrnshrf@gmail.com>
2023-03-09 11:23:38 -0500

    posmap stat added

--- src/memory_controller.c
+++ src/memory_controller.c
@@ -8114,8 +8114,11 @@ void export_csv(char * argv[]){
   fprintf(fp, "pos1_access,%d\n", pos1_access);
   fprintf(fp, "pos2_access,%d\n", pos2_access);
   // fprintf(fp, "plb_hit0,%f%%\n", 100*(double)plb_hit[0]/plbaccess[0]);
-  // fprintf(fp, "plb_hit1,%f%%\n", 100*(double)plb_hit[1]/plbaccess[1]);
-  // fprintf(fp, "plb_hit2,%f%%\n", 100*(double)plb_hit[2]/plbaccess[2]);
+  fprintf(fp, "plb_hit1,%f%%\n", 100*(double)plb_hit[1]/plbaccess[1]);
+  fprintf(fp, "plb_hit2,%f%%\n", 100*(double)plb_hit[2]/plbaccess[2]);
+  fprintf(fp, "posmap%%,%f%%\n", 100*(double)(pos1_access + pos1_access)/ringctr);
+  fprintf(fp, "data%%,%f%%\n", 100*(double)(ringctr - pos1_access - pos1_access)/ringctr);
+
   // fprintf(fp, "plb_hit0,%lld\n", plb_hit[0]);
   // fprintf(fp, "plb_hit1,%lld\n", plb_hit[1]);
   // fprintf(fp, "plb_hit2,%lld\n", plb_hit[2]);
@@ -8634,6 +8637,9 @@ void export_csv(char * argv[]){
   // }
   
   fprintf (fp, "dup_renewal, %lld\n", dup_renewal);
+  fprintf (fp, "dup_renew%%, %f%%\n", 100*(double)dup_renewal/ringctr);
+  fprintf(fp, "path_per_acc,%f%%\n", 100*(double)ringctr/invokectr);
+
   
   fclose(fp);
 }
