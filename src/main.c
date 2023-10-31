a2e997cf7c5fca072284068cfa21da7b28272daa
mhrnshrf <mhrnshrf@gmail.com>
2023-10-30 20:40:00 -0400

    1m 100k per print

--- src/main.c
+++ src/main.c
@@ -1737,7 +1737,7 @@ int main(int argc, char * argv[])
 				// printf("cache enable if: @ trace %d\n", tracectr);
 				while ((no_miss_occured && !expt_done) || (!SIM_ENABLE_VAR && tracectr < TRACE_SIZE-3) ) //  && tracectr <= endpoint
 				{
-					if (tracectr % 1000000 == 0)
+					if (tracectr % 100000 == 0)
 					{
 						printf("@%lld \n", tracectr);
 					}
@@ -2489,7 +2489,7 @@ int main(int argc, char * argv[])
 	// Mehrnoosh.
   }
 
-
+export_csv(argv);
   /* Code to make sure that the write queue drain time is included in
      the execution time of the thread that finishes last. */
   maxtd = time_done[0];
